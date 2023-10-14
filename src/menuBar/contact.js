c9b0c55f932d38402252729636dc7d161d4a56f0
AD-Costut <costut_ad05@yahoo.com>
2023-10-15 02:02:44 +0300

    Added contact page.

--- src/menuBar/contact.js
+++ src/menuBar/contact.js
@@ -4,6 +4,13 @@ export const Contact = () => {
   return (
     <div>
       <Welcome />
+      <div className="telefon">
+        Telefon: 0256 404 354; Email: simona.stanjic@upt.ro
+      </div>
+      <div className="adresa">
+        Adresă: Aleea Studenţilor 2, 300551 Timişoara, (la intersecţia cu Aleea
+        F.C. Ripensia).
+      </div>
     </div>
   );
 };
