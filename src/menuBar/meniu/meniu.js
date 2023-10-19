8a5ac66e980f3b057c06a0e99a97610e6c4f0428
AD-Costut <costut_ad05@yahoo.com>
2023-10-20 00:46:37 +0300

    Fixed menu selected and padding buttom.

--- src/menuBar/meniu/meniu.js
+++ src/menuBar/meniu/meniu.js
@@ -1,31 +1,66 @@
-import { React, useState } from "react";
+import { React, useState, useRef, useEffect } from "react";
 import Card from "./card";
 import { dataMeniulStandard } from "./dataMeniulStandard";
 import { dataMeniulZilei } from "./dataMeniulZilei";
 
 export const Meniu = () => {
   const [changeMenuData, setChangeMenuData] = useState(dataMeniulZilei);
+  const [showVisualMenuSelected, setShowVisualMenuSelected] =
+    useState("meniulZilei");
+
+  const menuDataRef = useRef(null);
+
+  useEffect(() => {
+    const setMaxHeight = () => {
+      if (menuDataRef.current) {
+        const viewportHeight = window.innerHeight;
+        const menuLayout = menuDataRef.current.parentElement;
+        const menuLayoutHeight = menuLayout.getBoundingClientRect().height;
+
+        const maxHeight =
+          viewportHeight -
+          (menuLayoutHeight - menuDataRef.current.offsetHeight);
+
+        menuDataRef.current.style.maxHeight = `${maxHeight}px`;
+      }
+    };
+
+    setMaxHeight();
+    window.addEventListener("resize", setMaxHeight);
+
+    return () => {
+      window.removeEventListener("resize", setMaxHeight);
+    };
+  }, []);
   return (
     <div className="menu-layout">
       <div className="parent">
         <a
-          className="child"
+          className={
+            showVisualMenuSelected === "meniulZilei" ? "clicked-menu" : "child"
+          }
           onClick={() => {
             setChangeMenuData(dataMeniulZilei);
+            setShowVisualMenuSelected("meniulZilei");
           }}
         >
           Meniul zilei
         </a>
         <a
-          className="child"
+          className={
+            showVisualMenuSelected === "meniulStandard"
+              ? "clicked-menu"
+              : "child"
+          }
           onClick={() => {
             setChangeMenuData(dataMeniulStandard);
+            setShowVisualMenuSelected("meniulStandard");
           }}
         >
           Meniul standard
         </a>
       </div>
-      <div className="menu-data">
+      <div className="menu-data" ref={menuDataRef}>
         {changeMenuData.map((item, index) => (
           <Card
             key={index}
