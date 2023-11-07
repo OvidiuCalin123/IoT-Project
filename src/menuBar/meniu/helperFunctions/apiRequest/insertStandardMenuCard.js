1159fa37f89085674be42cf285d82f2479e5a7c9
AD-Costut <costut_ad05@yahoo.com>
2023-11-07 20:06:57 +0200

    Salvare

---
+++ src/menuBar/meniu/helperFunctions/apiRequest/insertStandardMenuCard.js
@@ -0,0 +1,39 @@
+export const insertStandardMenuCard = (
+  token,
+  { title, description, priceForUPT, priceOutsidersUPT, image }
+) => {
+  const apiUrl = `https://localhost:7239/api/StandardMenu`;
+
+  const formData = new FormData();
+  console.log({ title, description, priceForUPT, priceOutsidersUPT, image });
+  formData.append("image", image);
+  formData.append("title", title);
+  formData.append("description", description);
+  formData.append("priceForUPT", priceForUPT);
+  formData.append("priceOutsidersUPT", priceOutsidersUPT);
+
+  return fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: formData,
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject(
+          `Error updating menu item: ${response.status} - ${response.statusText}`
+        );
+      }
+    })
+    .then((updatedData) => {
+      console.log("Menu item updated:", updatedData);
+      return updatedData;
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error.message);
+      return Promise.reject(error);
+    });
+};
